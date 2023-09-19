76931d776e256855a741084ab4e9400e437f72a4
OlegOKovalyov <koa2003@ukr.net>
2023-09-19 11:22:42 +0300

    1.0.3 Rename callback function and add comments

--- itip-cat-slider.php
+++ itip-cat-slider.php
@@ -4,7 +4,7 @@
  * Plugin Name:       ITIP Product Category Slider
  * Plugin URI:        https://itipteam.com/
  * Description:       Add slider effect to category product image
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 5.0
  * Requires PHP:      7.0
  * Author:            Oleg Kovalyov
@@ -23,23 +23,26 @@ add_action( 'wp_enqueue_scripts', 'itip_cat_slider_scripts' );
 
 // Customize WooCommerce shop page product thumbnails
 // Make slider effect on product thumbnail with additional image from ACF field 'slider_image'
-function replacing_template_loop_product_thumbnail() {
+function itip_replacing_template_loop_product_thumbnail() {
 
-	function itip_wc_template_loop_product_replaced_thumb() {
+	function wc_template_loop_product_replaced_thumb() {
+
 		$product_id = get_the_ID();
 
 		$thumbnail_url = get_the_post_thumbnail_url($product_id, 'thumbnail');
 
+		// Get additional image URL from ACF field in Edit Product page
 		$additional_image_url = get_field( 'slider_image', $product_id );
 		$additional_image = wp_get_attachment_image_src( $additional_image_url, 'medium' );
 
-		if ( $additional_image_url && is_product_category() ) {
-			echo '<img width="" height="" src="' . $additional_image_url . '" class="overlay-content" alt="" decoding="async" loading="lazy">';
+		if ( $additional_image_url && is_product_category() ) { // On category page only
+			echo '<img width="" height="" src="' . $additional_image_url .
+				'" class="overlay-content" alt="" decoding="async" loading="lazy">';
 		} else {
 
 		}
 	}
-	add_action( 'woocommerce_before_shop_loop_item_title', 'itip_wc_template_loop_product_replaced_thumb', 10 );
+	add_action( 'woocommerce_before_shop_loop_item_title', 'wc_template_loop_product_replaced_thumb', 10 );
 }
 
-add_action( 'woocommerce_init', 'replacing_template_loop_product_thumbnail' );
+add_action( 'woocommerce_init', 'itip_replacing_template_loop_product_thumbnail' );
